d274892c67b02b8f0aac09527997881f4b3da889
Otavio Miklos <otaviomiklos@gmail.com>
2023-11-22 09:34:19 -0300

    better visuals and add a function to add channels

--- popup_page/popup.js
+++ popup_page/popup.js
@@ -1,26 +1,46 @@
 let channelList = ["felps", "yasminhalves", "lhardoo"];
+let alreadyAdded = [];
+
 const channelsDiv = document.getElementById("channelListDiv");
-
-channelList.forEach(channel => {
-    let link = document.createElement('a');
-    link.classList.add("channel");
-    link.href = `https://www.twitch.tv/${channel}`;
-    link.innerText = channel[0].toUpperCase() + channel[1].toLowerCase();
-    link.addEventListener('click', () => {
-        openChannel(channel);
-    })
-
-    channelsDiv.appendChild(link);
-})
-
-form = document.getElementById('form');
-input = document.getElementById('input');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const btnAdd = document.querySelector(".button-add");
 input.focus();
+
+btnAdd.addEventListener('click', () => {
+    channelList.push(input.value);
+    renderChannels();
+});
 
 form.addEventListener('submit', () => {
     channel = input.value;
     openChannel(channel);
-})
+});
+
+
+function addChannel(){
+    channelList.push(input.value);
+    renderChannels();
+}
+
+renderChannels();
+
+function renderChannels(){
+    channelList.forEach(channel => {
+        if(alreadyAdded.includes(channel)) return;
+        alreadyAdded.push(channel);
+
+        let link = document.createElement('a');
+        link.classList.add("channel");
+        link.href = `https://www.twitch.tv/${channel}`;
+        link.innerText = channel;
+        link.addEventListener('click', () => {
+            openChannel(channel);
+        });
+    
+        channelsDiv.appendChild(link);
+    });
+}
 
 function openChannel(channel){
     chrome.tabs.create(
